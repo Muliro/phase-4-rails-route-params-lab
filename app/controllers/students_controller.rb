be87aa6028882b7907970b3b5cc137289c498a3c
muliro <mulirokhaemba@gmail.com>
2022-09-27 01:37:32 +0300

    dynamic

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
+
+
+
+
+
+
 class StudentsController < ApplicationController
 
   def index
     students = Student.all
     render json: students
+  end
+
+  def grade
+    student = Student.find_by(id: params[:id])
+    render json: student
   end
 
 end
